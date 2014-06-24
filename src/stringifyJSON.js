14f9ed40cb51344a993bd2d822cef56a84fd26ce
JonathanWarrick <jonathan.warrick@gmail.com>
2014-06-24 17:12:04 -0400

    Added switch statements to handle string, number types. Started working on object type (uses recursion).

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,21 +9,42 @@
 // Returns a string!
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // Create starting point for string
   var stringifiedObj = '{';
+  
+  // Iterate through each property in the object
+  _.each(obj, function(value, key, collection) {
+    // add key in quotations
+    stringifiedObj = stringifiedObj + '"' + key + '":';
+    
+    // create switch statement to handle various types of data
+    var typeOfVal = typeof value;
+    switch(typeOfVal) {
+      case "string":
+        stringifiedObj = stringifiedObj + '"' + value + '"';
+        break;
 
-  _.each(obj, function(value, key, collection) { 	
-  	stringifiedObj = stringifiedObj + '"' + key + '":"' + value + '"';
- 
-  	if (key === Object.keys(collection)[Object.keys(collection).length - 1]) {
-  		stringifiedObj = stringifiedObj + '}';
-  	}
+      case "number":
+        stringifiedObj = stringifiedObj + value;
+        break;
 
-  	else {
-  		stringifiedObj = stringifiedObj + ',';
-  	}
+      case "object":
+        stringifyJSON(collection);
+        break;
+
+      default:
+        stringifiedObj = stringifiedObj;
+    }
+
+    if (key === Object.keys(collection)[Object.keys(collection).length - 1]) {
+      stringifiedObj = stringifiedObj + '}';
+    }
+
+    else {
+      stringifiedObj = stringifiedObj + ',';
+    }
   });
-
+ 
   return stringifiedObj;
 };
 
@@ -36,5 +57,32 @@ var contact = new Object();
 contact.firstname = "Jesper";
 contact.surname = "Aaberg";
 contact.phone = ["555-0100", "555-0120"];
+
+var testObj = { 
+  test1: "Jonathan", 
+  test2: 234, 
+  test3: "Test", 
+  test4: { newtest1: "JJ", newtest2: "JJJ" }, 
+  test5: [1,2,3,"Jonathan"] 
+};
+
+var person = {
+    name: "Jim Cowart",
+    location: {
+        city: {
+            name: "Chattanooga",
+            population: 167674
+        },
+        state: {
+            name: "Tennessee",
+            abbreviation: "TN",
+            population: 6403000
+        }
+    },
+    company: "appendTo",
+};
+
+// returns {"name":"Jim Cowart","location":{"city":{"name":"Chattanooga","population":167674},"state":{"name":"Tennessee","abbreviation":"TN","population":6403000}},"company":"appendTo"}
+
 
 // returns {"firstname":"Jesper","surname":"Aaberg","phone":["555-0100","555-0120"]}
