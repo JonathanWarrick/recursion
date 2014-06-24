9942b5c487a7936d462c0dbd00dc734c1225288f
JonathanWarrick <jonathan.warrick@gmail.com>
2014-06-24 15:05:16 -0400

    Wrote initial structure for _.each() function go to through top-level objects. Outputs string with minor caveats.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,39 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+
+// NOTES ABOUT JSON.stringify():
+// All keys in objects are returned in quotations.
+// All values in objects are returned as fed-in (strings don't have double quotes, numbers, etc. don't have single quotes).
+// Returns a string!
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  var stringifiedObj = '{';
+  var objLength;
+  if (Array.isArray(obj)) {
+  	objLength = obj.length;
+  }
+
+  else {
+  	objLength = Object.keys(obj).length;
+  }
+
+  _.each(obj, function(value, key, collection) { 	
+  	stringifiedObj = stringifiedObj + '"' + key + '":"' + value + '",';
+  });
+
+  return stringifiedObj;
 };
+
+
+
+
+
+
+var contact = new Object();
+contact.firstname = "Jesper";
+contact.surname = "Aaberg";
+contact.phone = ["555-0100", "555-0120"];
+
+// returns {"firstname":"Jesper","surname":"Aaberg","phone":["555-0100","555-0120"]}
