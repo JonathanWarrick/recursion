d7c43ca73cb23d9b94a078effd2b0fcba863f640
JonathanWarrick <jonathan.warrick@gmail.com>
2014-06-25 09:42:20 -0400

    Added initial structure of getElementsByClassName(). Not working currently; need to understand element.childNodes vs. element.Children better.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
+// Want to return an array-like object of child elements which have the given class name.
+// Can be used starting with document, to return ALL elements with class name, or on a specific element, to get direct descendants.
+
 var getElementsByClassName = function(className){
   // your code here
+  var results = [];
+  function checkForClass(element) {
+  	var childNodes = element.childNodes;
+  	_.each(childNodes, function(value, key, collection) {
+  		if (_.contains(collection[key].classList, className)) {
+  			results.push(collection);
+  		};
+
+  		if (collection.childNodes.length > 0) {
+  			checkForClass(collection);
+  		}
+  	});
+  }
+
+  checkForClass(document);
+  return results;
 };
